947d83f56116898cd9f5825bf5d5c9b90ac1457b
Eason Zhang _ s1121432 <s1121432@mail.yzu.edu.tw>
2025-11-14 15:10:12 +0800

    feat(cxx): Improve integer hash to handle edge case

--- Assignment/AssignmentIV/CXX/hash_fn.cpp
+++ Assignment/AssignmentIV/CXX/hash_fn.cpp
@@ -15,7 +15,13 @@
 
 int myHashInt(int key, int m) {
     // TODO: replace with your own design
-    return key % m;  // basic division method
+    if (m <= 0) return 0;
+
+    // 2. 計算原本邏輯下的INDEX
+    int index = key % m;
+
+    // 3. 處理負數情況：如果INDEX是負的，將其調整到 [0, m-1] 的範圍內。
+    return (index < 0) ? (index + m) : index;
 }
 
 int myHashString(const std::string& str, int m) {
