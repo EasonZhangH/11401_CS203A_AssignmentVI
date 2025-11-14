3f91da02a14c2d73f2ba9cb3be57c12794824b59
Eason Zhang _ s1121432 <s1121432@mail.yzu.edu.tw>
2025-11-14 16:25:13 +0800

    feat(cxx): Convert C version to C++ version

--- Assignment/AssignmentIV/CXX/hash_fn.cpp
+++ Assignment/AssignmentIV/CXX/hash_fn.cpp
@@ -17,11 +17,16 @@ int myHashInt(int key, int m) {
     // TODO: replace with your own design
     if (m <= 0) return 0;
 
+    unsigned int x = static_cast<unsigned int>(key);
+    x = x * 2654435761u;
+    x ^= (x >> 16);
+
     // 2. 計算原本邏輯下的INDEX
-    int index = key % m;
+    int index = x % m;
 
     // 3. 處理負數情況：如果INDEX是負的，將其調整到 [0, m-1] 的範圍內。
-    return (index < 0) ? (index + m) : index;
+    //return (index < 0) ? (index + m) : index;
+    return static_cast<int>(index);
 }
 
 int myHashString(const std::string& str, int m) {
@@ -32,10 +37,12 @@ int myHashString(const std::string& str, int m) {
     if ( m <= 0 || str.empty() )
         return 0; // edge case 
 
-    for (char c : str)
-        // 將字元轉換為 1-26 的數字 (假設只有小寫字母)
-        hash = (hash + (c - 'a' + 1) ) % m;
+    const unsigned long long base = 131;
 
+    for (char c : str) {
+        // 將字元轉換為 1-26 的數字 (假設只有小寫字母)
+        hash = (hash*base + (unsigned char)c ) % m;
+    }
     //確保結果為正數並回傳
     return static_cast<int>(hash);
 }
