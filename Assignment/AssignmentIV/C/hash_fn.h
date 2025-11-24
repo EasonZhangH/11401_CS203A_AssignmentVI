ae84ce72af82cc0d4a074ea293cba78da4f8793e
jia-xiangZhang <165023065+EasonZhangH@users.noreply.github.com>
2025-11-24 10:17:32 +0800

    Update developer information and date in hash_fn.h

--- Assignment/AssignmentIV/C/hash_fn.h
+++ Assignment/AssignmentIV/C/hash_fn.h
@@ -7,9 +7,9 @@
     This file contains the hash functions for integer and string keys.
 
    Development History:
-    - 2025/11/17: Initial implementation
+    - 2025/11/24: Initial implementation
 
-   Developer: Yu-Feng Huang <yfhuang@saturn.yzu.edu.tw>
+   Developer: Eason Zhang <s1121432@mail.yzu.edu.tw>
  */
 #ifndef HASH_FN_H
 #define HASH_FN_H
